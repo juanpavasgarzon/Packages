a2cbbc9cda2307416d6822de36263c2389f4aa3a
Juan Pavas <garzonp2001@gmail.com>
2024-09-24 11:14:47 -0500

    Ajustments

--- UnitOfWork.Example/Program.cs
+++ UnitOfWork.Example/Program.cs
@@ -7,16 +7,16 @@ var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingletonContext(new TenantContext { TenantId = "MyTenant2" });
+builder.Services.AddSingletonContext(new TenantContext { TenantId = "MyTenant" });
 builder.Services.AddUnitOfWork<UnitOfWorkContext, UnitOfWorkConfigurator>(ServiceLifetime.Singleton);
 
 var app = builder.Build();
 
 var unitOfWork = app.Services.GetRequiredService<IUnitOfWork>();
 var repository = unitOfWork.GetRepository<MyEntity>();
-var entity = await repository.GetByIdAsync(17);
+var entities = await repository.GetAllAsync();
 
-Console.WriteLine(entity);
+Console.WriteLine(entities);
 
 await unitOfWork.SaveChangesAsync();
 
