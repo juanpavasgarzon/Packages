2698abf9a1e3f1326fff6367b81b4ccb69da6777
Juan Pavas <garzonp2001@gmail.com>
2024-09-26 23:44:25 -0500

    Base entity

--- UnitOfWork.Example/BaseEntity.cs
+++ UnitOfWork.Example/BaseEntity.cs
@@ -7,11 +7,8 @@ namespace UnitOfWork.Example;
 public abstract class BaseEntity : ITimestamps, ITenancy, ISoftDelete, ICorrelated
 {
     [Timestamp] public DateTime CreatedAt { get; set; }
-
     [Timestamp] public DateTime? UpdatedAt { get; set; }
-
     [MaxLength(255)] public string TenantId { get; set; } = string.Empty;
-
     [DefaultValue(false)] public bool IsDeleted { get; set; }
     public DateTime? DeletedAt { get; set; }
     [MaxLength(255)] public string CorrelationId { get; set; } = string.Empty;
